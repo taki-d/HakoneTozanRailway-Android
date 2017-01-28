7edb3817b0d72568f2df152a3fec5cb9e9916c55
taki-d <taka410199@outlook.jp>
2017-01-28 14:51:44 +0900

    [add]application nameとapplication versionを追加

--- main.cpp
+++ main.cpp
@@ -9,6 +9,8 @@ int main(int argc, char *argv[])
     //Support High DPI Device
     QGuiApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QGuiApplication app(argc, argv);
+    QGuiApplication::setApplicationName("HTT Bread");
+    QGuiApplication::setApplicationVersion("beta0.1");
 
     //if you want to register class to QtQuick, please write like this!
 //    qmlRegisterType<std::vector>("vector",1,0,"vector");
